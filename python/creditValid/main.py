af993cc690370552611a61365e34816831d4a6b3
GrimDeath <82227028+Yami-Kawaii@users.noreply.github.com>
2023-07-26 14:20:01 +0800

    Update main.py

--- python/creditValid/main.py
+++ python/creditValid/main.py
@@ -3,7 +3,7 @@ def getBIN(digit,valid):
         if valid:
             if digit[0] == '4':
                 return 'Visa'
-            elif digit == '5':
+            elif digit[0] == '5':
                 return 'Mastercard'
             else:
                 return 'Unknown'
@@ -16,14 +16,10 @@ def getBIN(digit,valid):
 def creditChecker(ccn):
     evenNum = [int(ccn[num]) for num in range(len(ccn)) if int(num)%2==0]
     oddNum = [int(ccn[num]) for num in range(len(ccn)) if int(num)%2==1]
-    timeCard = [num*2 for num in evenNum]
-    for i in range(len(timeCard)-1):
-        str(evenNum[i])
-        if len(str(timeCard[i])) > 1:
-            timeCard[i] = int(str(timeCard[i])[0])+int(str(timeCard[i])[1])
+    timeCard = [num * 2 if num * 2 < 10 else num * 2 - 9 for num in evenNum]
     return (sum(oddNum)+sum(timeCard))%10==0
     
-cc='3566002020360505'
+cc='5555 5555 5555 4444'
 valid = creditChecker(cc.replace(' ',''))
 company = getBIN(cc,valid)
 print(f'{cc}|{valid}|{company}')
